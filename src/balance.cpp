497e118cf5022d63729c58e9a28de6cf2f3f7bcc
Peter Dunson <90647939+peterdunson@users.noreply.github.com>
2025-10-16 13:59:01 -0400

    submit
    submit proj5, balance parens

--- src/balance.cpp
+++ src/balance.cpp
@@ -2,13 +2,12 @@
 #include <string>
 
 static bool is_balanced_rec(const std::string& s, int idx, int open) {
-    // TODO: implement recursively.
-    // - If open < 0 => false
-    // - If idx == s.size() => open == 0
-    // - If '(' => recurse open+1
-    // - If ')' => recurse open-1
-    // - else ignore
-    return false;
+    if (open < 0) return false;
+    if (idx == (int)s.size()) return open == 0;
+    char c = s[idx];
+    if (c == '(') return is_balanced_rec(s, idx+1, open+1);
+    if (c == ')') return is_balanced_rec(s, idx+1, open-1);
+    return is_balanced_rec(s, idx+1, open);
 }
 
 bool is_balanced(const std::string& s) {
